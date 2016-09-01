dbf3f2ae619d47d1988bdf28f317c9484cf4c623
Scott Weiss <scott.weiss@emc.com>
2016-09-01 02:08:21 -0400

    make port cf-friendly
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- example_go_static_fileserver/example_go_app.go
+++ example_go_static_fileserver/example_go_app.go
@@ -7,14 +7,18 @@ import (
 )
 
 func main() {
+	port := ":8080"
+	if p := os.Getenv("PORT"); p != "" {
+		port = ":"+p
+	}
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	fmt.Printf("listening on port :8080")
+	fmt.Printf("listening on port "+port)
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("cwd: %s", wd)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
